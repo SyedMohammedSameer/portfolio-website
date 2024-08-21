d10cdb076faa6fec5cb190d6cb5f40a086608fdf
SyedMohammedSameer <mohammedsameersyed1@gmail.com>
2024-08-21 22:54:36 +0530

    second commit

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -7,9 +7,9 @@ function Type() {
       options={{
         strings: [
           "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "ML Engineer",
+          "Data Scientist",
+          "AI Engineer",
         ],
         autoStart: true,
         loop: true,
