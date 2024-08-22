7bce588ea40c686883798820866a01bcccb8c8bb
SyedMohammedSameer <mohammedsameersyed1@gmail.com>
2024-08-22 15:55:06 +0530

    errorfixed

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import { CgCPlusPlus } from "react-icons/cg";
 import { DiPython } from "react-icons/di";
-import { SiNumpy, SiPandas, SiTensorflow, SiScikitlearn } from "react-icons/si";
+import { SiNumpy, SiPandas, SiScikitlearn, SiTensorflow } from "react-icons/si";
 
 function Techstack() {
   return (
@@ -18,9 +18,6 @@ function Techstack() {
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPandas />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <FaChartLine />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiScikitlearn />
